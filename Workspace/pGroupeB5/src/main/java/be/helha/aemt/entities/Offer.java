aa2cf408f544f8b7d18b1f02b3c85976cc5241ff
Mithirsan <la162765@student.helha.be>
2020-01-13 23:29:28 +0100

    Offer and OfferDAO
    with child classes

--- Workspace/pGroupeB5/src/main/java/be/helha/aemt/entities/Offer.java
+++ Workspace/pGroupeB5/src/main/java/be/helha/aemt/entities/Offer.java
@@ -4,59 +4,73 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 
 import be.helha.aemt.model.OfferType;
 import be.helha.aemt.model.SectionEconomicHELHaMons;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED) 
 public class Offer {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	private String company, email, contact, describtion;
+	private String company, title, contact, email, offerDescription, publishingDate;
 	
 	@ManyToOne
 	private Address adress;
 	
-	private SectionEconomicHELHaMons targetSection;
-	
 	private OfferType offerType;
+	
+	private SectionEconomicHELHaMons targetSection;
 	
 	public Offer() { }
 
-	public Offer(String company, String email, String contact, String describtion, Address adress,
-			SectionEconomicHELHaMons targetSection, OfferType offerType) {
+	public Offer(String company, String title, String contact, String email, String offerDescription,
+			String publishingDate, Address adress, OfferType offerType, SectionEconomicHELHaMons targetSection) {
 		super();
 		this.company = company;
-		this.email = email;
+		this.title = title;
 		this.contact = contact;
-		this.describtion = describtion;
+		this.email = email;
+		this.offerDescription = offerDescription;
+		this.publishingDate = publishingDate;
 		this.adress = adress;
-		this.targetSection = targetSection;
 		this.offerType = offerType;
+		this.targetSection = targetSection;
 	}
 
-	public Offer(Integer id, String company, String email, String contact, String describtion, Address adress,
-			SectionEconomicHELHaMons targetSection, OfferType offerType) {
+	public Offer(Integer id, String company, String title, String contact, String email, String offerDescription,
+			String publishingDate, Address adress, OfferType offerType, SectionEconomicHELHaMons targetSection) {
 		super();
 		this.id = id;
 		this.company = company;
-		this.email = email;
+		this.title = title;
 		this.contact = contact;
-		this.describtion = describtion;
+		this.email = email;
+		this.offerDescription = offerDescription;
+		this.publishingDate = publishingDate;
 		this.adress = adress;
-		this.targetSection = targetSection;
 		this.offerType = offerType;
+		this.targetSection = targetSection;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((adress == null) ? 0 : adress.hashCode());
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((contact == null) ? 0 : contact.hashCode());
+		result = prime * result + ((offerDescription == null) ? 0 : offerDescription.hashCode());
+		result = prime * result + ((offerType == null) ? 0 : offerType.hashCode());
+		result = prime * result + ((publishingDate == null) ? 0 : publishingDate.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((targetSection == null) ? 0 : targetSection.hashCode());
 		return result;
 	}
 
@@ -69,10 +83,39 @@ public class Offer {
 		if (getClass() != obj.getClass())
 			return false;
 		Offer other = (Offer) obj;
-		if (id == null) {
-			if (other.id != null)
+		if (adress == null) {
+			if (other.adress != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!adress.equals(other.adress))
+			return false;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (contact == null) {
+			if (other.contact != null)
+				return false;
+		} else if (!contact.equals(other.contact))
+			return false;
+		if (offerDescription == null) {
+			if (other.offerDescription != null)
+				return false;
+		} else if (!offerDescription.equals(other.offerDescription))
+			return false;
+		if (offerType != other.offerType)
+			return false;
+		if (targetSection != other.targetSection)
+			return false;
+		if (publishingDate == null) {
+			if (other.publishingDate != null)
+				return false;
+		} else if (!publishingDate.equals(other.publishingDate))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
 			return false;
 		return true;
 	}
@@ -93,12 +136,12 @@ public class Offer {
 		this.company = company;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public String getContact() {
@@ -109,12 +152,28 @@ public class Offer {
 		this.contact = contact;
 	}
 
-	public String getDescribtion() {
-		return describtion;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setDescribtion(String describtion) {
-		this.describtion = describtion;
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getOfferDescription() {
+		return offerDescription;
+	}
+
+	public void setOfferDescription(String offerDescription) {
+		this.offerDescription = offerDescription;
+	}
+
+	public String getPublishingDate() {
+		return publishingDate;
+	}
+
+	public void setPublishingDate(String publishingDate) {
+		this.publishingDate = publishingDate;
 	}
 
 	public Address getAdress() {
@@ -125,14 +184,6 @@ public class Offer {
 		this.adress = adress;
 	}
 
-	public SectionEconomicHELHaMons getTargetSection() {
-		return targetSection;
-	}
-
-	public void setTargetSection(SectionEconomicHELHaMons targetSection) {
-		this.targetSection = targetSection;
-	}
-
 	public OfferType getOfferType() {
 		return offerType;
 	}
@@ -141,10 +192,18 @@ public class Offer {
 		this.offerType = offerType;
 	}
 
+	public SectionEconomicHELHaMons getTargetSection() {
+		return targetSection;
+	}
+
+	public void setTargetSection(SectionEconomicHELHaMons targetSection) {
+		this.targetSection = targetSection;
+	}
+
 	@Override
 	public String toString() {
-		return "Offer [id=" + id + ", company=" + company + ", email=" + email + ", contact=" + contact
-				+ ", describtion=" + describtion + ", adress=" + adress + ", targetSection=" + targetSection
-				+ ", offerType=" + offerType + "]";
+		return "Offer [id=" + id + ", company=" + company + ", title=" + title + ", contact=" + contact + ", email="
+				+ email + ", offerDescription=" + offerDescription + ", publishingDate=" + publishingDate + ", adress="
+				+ adress + ", offerType=" + offerType + ", targetSection=" + targetSection + "]";
 	}
 }
