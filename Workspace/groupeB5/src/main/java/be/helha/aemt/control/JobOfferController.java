773271c3b7c5d18826d6fadab85dfed717af244d
JUB-059 <2007f005@student.helha.be>
2020-01-15 17:48:43 +0100

    Pages

--- Workspace/groupeB5/src/main/java/be/helha/aemt/control/JobOfferController.java
+++ Workspace/groupeB5/src/main/java/be/helha/aemt/control/JobOfferController.java
@@ -1,6 +1,7 @@
 package be.helha.aemt.control;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -11,7 +12,10 @@ import be.helha.aemt.ejb.ManageAddressEJB;
 import be.helha.aemt.ejb.ManageJobOfferEJB;
 import be.helha.aemt.entities.Address;
 import be.helha.aemt.entities.JobOffer;
+import be.helha.aemt.entities.Offer;
+import be.helha.aemt.entities.User;
 import be.helha.aemt.model.OfferType;
+import be.helha.aemt.model.SectionEconomicHELHaMons;
 
 @SessionScoped
 @Named
@@ -19,6 +23,8 @@ public class JobOfferController implements Serializable {
 	
 	@EJB
 	private ManageJobOfferEJB beanJO;
+	
+	private SectionEconomicHELHaMons targetSection;
 	
 	private JobOffer jobOffer;
 	
@@ -32,6 +38,32 @@ public class JobOfferController implements Serializable {
 		this.jobOffer = jobOffer;
 	}
 
+	
+	/* MODELE VENANT DE "UserController", A SUPPRIMER SI "filterOffer()" FONCTIONNE
+	public List<User> filterUser()
+	{
+		List<User> tmp = new ArrayList<User>();
+		for (User user : doSelectValidNonAdminUser()) {
+			if(user.getGraduationSection()==targetSection)
+			{
+				tmp.add(user);
+			}
+		}
+		return tmp;
+	}*/
+	
+	public List<JobOffer> filterOffer() {
+		List<JobOffer> tmp = new ArrayList<>();
+		for (JobOffer offer : doSelectAllJobOfferAsList()) {
+			if (offer.getTargetSection()==targetSection) {
+				tmp.add(offer);
+			}
+		}
+		return tmp;
+	}
+	
+	
+	
 
 	public JobOffer getJobOffer() {
 		return jobOffer;
